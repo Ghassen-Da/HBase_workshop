193fe146da3f0e13299a20c0679bcecb89c5e243
Ghassen-Da <ghassendaoud99@gmail.com>
2022-04-15 13:42:49 +0100

    commit

--- src/main/java/tn/insat/tp4/HbaseSparkProcess.java
+++ src/main/java/tn/insat/tp4/HbaseSparkProcess.java
@@ -5,9 +5,14 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+import scala.Tuple2;
 
 public class HbaseSparkProcess {
 
@@ -21,8 +26,14 @@ public class HbaseSparkProcess {
         JavaPairRDD<ImmutableBytesWritable, Result> hBaseRDD =
                 jsc.newAPIHadoopRDD(config, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
 
-        System.out.println("nombre d'enregistrements: "+hBaseRDD.count());
-
+        JavaRDD<String> javaRDD = hBaseRDD.map((Function<Tuple2<ImmutableBytesWritable, Result>, String>) tuple->{
+        	Result result = tuple._2;
+        	return Bytes.toString(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("price")));
+        });
+        
+        String sumResult = javaRDD.reduce((value1, value2)-> String.valueOf(Float.parseFloat(value1)+Float.parseFloat(value2)));
+        
+        System.out.println("The Sum of products is: " + sumResult);
     }
 
     public static void main(String[] args){
